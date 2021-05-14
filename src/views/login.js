e507aabef242e86dca7d66207c6ccd17cb8e13ac
Mikael-R <mikarg9@gmail.com>
2021-05-14 20:01:52 -0300

    remove html desnecessario

--- src/views/login.js
+++ src/views/login.js
@@ -1,84 +1,81 @@
-import React from 'react'
-import Card from '../components/card'
-import FormGroup from '../components/form-group'
-import { withRouter } from 'react-router-dom'
+import React from "react";
+import Card from "../components/card";
+import FormGroup from "../components/form-group";
+import { withRouter } from "react-router-dom";
 
-import UsuarioService from '../app/service/usuarioService'
-import LocalStorageService from '../app/service/localstorageService'
-import { mensagemErro } from '../components/toastr'
-import { AuthContext  } from '../main/provedorAutenticacao'
+import UsuarioService from "../app/service/usuarioService";
+// import LocalStorageService from '../app/service/localstorageService'
+import { mensagemErro } from "../components/toastr";
+import { AuthContext } from "../main/provedorAutenticacao";
 
-class Login extends React.Component{
+class Login extends React.Component {
+  state = {
+    email: "",
+    senha: "",
+  };
 
-    state = {
-        email: '',
-        senha: ''
-    }
+  constructor() {
+    super();
+    this.service = new UsuarioService();
+  }
 
-    constructor(){
-        super();
-        this.service = new UsuarioService();
-    }
+  entrar = () => {
+    this.service
+      .autenticar({
+        email: this.state.email,
+        senha: this.state.senha,
+      })
+      .then((response) => {
+        this.context.iniciarSessao(response.data);
+        this.props.history.push("/home");
+      })
+      .catch((erro) => {
+        mensagemErro(erro.response.data);
+      });
+  };
 
-    entrar = () => {
-        this.service.autenticar({
-            email: this.state.email,
-            senha: this.state.senha
-        }).then( response => {
-            this.context.iniciarSessao(response.data)
-            this.props.history.push('/home')
-        }).catch( erro => {
-           mensagemErro(erro.response.data)
-        })
-    }
+  prepareCadastrar = () => {
+    this.props.history.push("/cadastro-usuarios");
+  };
 
-    prepareCadastrar = () => {
-        this.props.history.push('/cadastro-usuarios')
-    }
-
-    render(){
-        return (
-
-            <div className="row">
-                <div className="col-md-6" style={ {position : 'relative', left: '300px'} }>
-                    <div className="bs-docs-section">
-                        <Card title="Login">
-                            <div className="row">
-                                <div className="col-lg-12">
-                                    <div className="bs-component">
-                                        <fieldset>
-                                            <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
-                                                <input type="email" 
-                                                    value={this.state.email}
-                                                    onChange={e => this.setState({email: e.target.value})}
-                                                    className="form-control" 
-                                                    id="exampleInputEmail1" 
-                                                    aria-describedby="emailHelp" 
-                                                    placeholder="Digite o Email" />
-                                            </FormGroup>
-                                            <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
-                                                <input type="password" 
-                                                        value={this.state.senha}
-                                                        onChange={e => this.setState({senha: e.target.value})}
-                                                        className="form-control" 
-                                                        id="exampleInputPassword1" 
-                                                        placeholder="Password" />
-                                            </FormGroup>
-                                            <button onClick={this.entrar} className="btn btn-success">
-                                                <i className="pi pi-sign-in"></i>Entrar</button>
-                                        </fieldset>
-                                    </div>
-                                </div>
-                            </div>
-                        </Card>
-                    </div>
-                </div>
-            </div>
-
-        )
-    }
+  render() {
+    return (
+      <div className="main row">
+        <Card class="card" title="Login">
+          <form>
+            <FormGroup label="Email:" htmlFor="exampleInputEmail1">
+              <input
+                type="email"
+                value={this.state.email}
+                required
+                onChange={(e) => this.setState({ email: e.target.value })}
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Seu email"
+              />
+            </FormGroup>
+            <FormGroup label="Senha:" htmlFor="exampleInputPassword1">
+              <input
+                type="password"
+                value={this.state.senha}
+                required
+                onChange={(e) => this.setState({ senha: e.target.value })}
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Sua senha"
+              />
+            </FormGroup>
+            <button onSubmit={this.entrar} className="btn btn-success">
+              <i className="pi pi-sign-in"></i>Entrar
+            </button>
+          </form>
+        </Card>
+      </div>
+    );
+  }
 }
 
-Login.contextType = AuthContext
+Login.contextType = AuthContext;
 
-export default withRouter( Login )
+export default withRouter(Login);
